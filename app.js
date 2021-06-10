5b8e66a005383bff415a27582eda6df8014da9fc
Ali3597 <76229456+Ali3597@users.noreply.github.com>
2021-06-10 16:51:15 +0000

    passage jwt token +++ connection asterisk

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const path = require('path');
-require ("./database");
+const path = require("path");
+const express = require("express");
+require("./database");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
 const app = express();
-exports.app = app;
-const morgan = require('morgan')
-var cookies = require("cookie-parser");
-const index =require('./routes');
-// variable des ports 
-const port = process.env.PORT || 3000;
+const server = app.listen(3000);
+module.exports = {
+  server,
+  app,
+};
+var ari = require('ari-client');
+var util = require('util');
+const {clientLoaded }= require('./asterisk/index')
+ari.connect('http://192.168.1.12:8088', 'asterisk', 'asterisk', clientLoaded);
 
-app.set('views',path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-require('./config/session.config');
-require('./config/passport.config');
-//middleware
-app.use(morgan('short'))
-app.use(cookies());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+require("./config/jwt.config");
+require("./config/socket.config");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: true  }));
-
-
-//route
-app.use(index);
-
-//gestion des erreurs 
-
-
-
-app.listen(port);
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
